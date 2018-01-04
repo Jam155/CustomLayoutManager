7bb14d57031ed2701f90a572f540c8dc75e0bc6e
James Lendrem <James@kinocreative.co.uk>
2018-01-04 19:15:49 +0000

    Add the image view into our viewholder.

--- app/src/main/java/net/tinad/jam/customlayoutmanager/viewholders/PictureViewHolder.java
+++ app/src/main/java/net/tinad/jam/customlayoutmanager/viewholders/PictureViewHolder.java
@@ -2,6 +2,9 @@ package net.tinad.jam.customlayoutmanager.viewholders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+
+import net.tinad.jam.customlayoutmanager.R;
 
 /**
  * Created by nemo on 29/12/17.
@@ -9,8 +12,13 @@ import android.view.View;
 
 public class PictureViewHolder extends RecyclerView.ViewHolder {
 
+    public ImageView imageView;
+
     public PictureViewHolder(View itemView) {
+
         super(itemView);
+        imageView = itemView.findViewById(R.id.image);
+
     }
 
 }
